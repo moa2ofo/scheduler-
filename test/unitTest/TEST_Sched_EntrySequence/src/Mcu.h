2cbd43676466f9829bb000eb28c2a452f4d1fd8d
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-11 11:32:16 +0200

    DELETE UNIT TESTS

--- test/unitTest/TEST_Sched_EntrySequence/src/Mcu.h
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-void Mcu_Initialize();
-
-void Mcu_ServiceWatchdogSow();
-
-void Mcu_ReloadSystick();
-
-uint32_t Mcu_GetSystemTime_u32();
--- test/unitTest/TEST_Sched_Task2/src/Sched_Task2.c
+++
@@ -1,6 +0,0 @@
-#include "Sched_Task2_help.h"
-
-/* FUNCTION TO TEST */
-void Sched_Task2(void) {
-  /* Task code for 20ms period */
-}
