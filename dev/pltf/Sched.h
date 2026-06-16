14e09f039b8996ffd16b62ab5484ac68f77e882e
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-16 11:12:35 +0200

    trigger git actions

--- dev/pltf/Sched.h
+++ dev/pltf/Sched.h
@@ -165,7 +165,6 @@
  * | SWDD_0011_CFG  | @ref SCHED_TASK2_SHIFT    | P        | uint32_t         | N/A    | N/A    | 1                  | 7                  | ms   | 7          | N/A            |
  * | SWDD_0012_CFG  | @ref SCHED_TASK3_SHIFT    | P        | uint32_t         | N/A    | N/A    | 1                  | 17                 | ms   | 17         | N/A            |
  *
- *
  * ### High-level interaction
  * @startuml
  * title Scheduler Module
