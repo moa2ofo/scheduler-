e08079d3b3fe80e846c7dea20a1989a16d921dda
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-16 13:52:46 +0200

    delete documents

--- dev/docs/html/Sched__Priv_8c.js
+++
@@ -1,8 +0,0 @@
-var Sched__Priv_8c =
-[
-    [ "Sched_Task0", "group__Sched.html#gae400bce4176ddab53753e642e759598b", null ],
-    [ "Sched_Task1", "Sched__Priv_8c.html#a10314d7837d80b3579c3b4267d144945", null ],
-    [ "Sched_Task2", "Sched__Priv_8c.html#a2903d59f824547c96cfde10d5356a158", null ],
-    [ "Sched_Task3", "Sched__Priv_8c.html#a0e7ef63cf946dd03f941e68381e6089e", null ],
-    [ "Sched_Task4", "Sched__Priv_8c.html#aed8620d4925861e52e20a1fe0e49126d", null ]
-];
