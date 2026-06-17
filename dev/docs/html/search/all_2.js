3c0d626d27f3f232e1938253a7babd49fc5c897f
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-17 13:27:16 +0200

    delete docs

--- dev/docs/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['data_20specification_0',['Data Specification',['../group__Sched.html#autotoc_md2',1,'']]],
-  ['deltatime_5fu32_1',['deltatime_u32',['../Sched_8c.html#a00304344cc73ea96d3ad1b85eefe2df2',1,'DeltaTime_u32(void):&#160;Sched.c'],['../group__Sched.html#gad96c7d7f2c095f2e4622bb18fb459c12',1,'DeltaTime_u32(void):&#160;Sched_Priv.h']]],
-  ['design_20and_20traceability_20matrix_2',['Static design and traceability matrix',['../group__Sched.html#autotoc_md0',1,'']]]
-];
--- dev/docs/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['taskhandler_5ft_0',['taskhandler_t',['../group__Sched.html#gadce47304ee8b44e4d240b01367c2f82a',1,'TaskHandler_t:&#160;Sched_Priv.h'],['../group__Sched.html#gadce47304ee8b44e4d240b01367c2f82a',1,'TaskHandler_t:&#160;Sched_Priv.h']]]
-];
