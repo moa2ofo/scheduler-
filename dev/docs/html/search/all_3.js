9363090d0d3c8cb988164e2d64dd58ea1dc30441
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-17 09:58:31 +0200

    delete docs

--- dev/docs/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['executependingtasks_0',['executependingtasks',['../Sched_8c.html#a75a48d5cf2208ddbe805fffb9af6df15',1,'ExecutePendingTasks(void):&#160;Sched.c'],['../group__Sched.html#ga2679ff4b078150030488c031d6aa0db9',1,'ExecutePendingTasks(void):&#160;Sched_Priv.h']]]
-];
--- dev/docs/html/test_2unitTest_2TEST__Sched__EntrySequence_2src_2Sched__Cfg_8h.js
+++
@@ -1,4 +0,0 @@
-var test_2unitTest_2TEST__Sched__EntrySequence_2src_2Sched__Cfg_8h =
-[
-    [ "Sched_GetRefTime_u32", "group__Sched.html#ga6db6318dec09fb3100bd27a4fe7981ff", null ]
-];
