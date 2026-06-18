79f649362a7b751c699f218c47b8f06e8451d70a
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-06-18 10:11:50 +0200

    Modified Scheduler disposition pltf/cfg

--- dev/cfg/Sched_Cfg.h
+++ dev/cfg/Sched_Cfg.h
@@ -4,14 +4,16 @@
 #include "Sched_Priv.h"
 #include <stdint.h>
 
-
+/**
+ * @name Task Cfg Periods [ms]
+ * @{
+ */
 #define SCHED_TASK1_SHIFT (2u)
 #define SCHED_TASK2_SHIFT (7u)
 #define SCHED_TASK3_SHIFT (17u)
 #define SCHED_TASK1_PERIOD (10u)
 #define SCHED_TASK2_PERIOD (20u)
 #define SCHED_TASK3_PERIOD (100u)
-
 /** @} */
 
 
@@ -40,13 +42,7 @@ void Sched_Task2(void);
 void Sched_Task3(void);
 #endif
 
-#if(SCHED_NUM_OF_TASKS > 4u)
-/**
- * @brief Task 4 entry point.
- * @ingroup Sched
- */
-void Sched_Task4(void);
-#endif
+
 /** @} */
 
 /** @} */
